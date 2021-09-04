b44feccbe5970a9ddd4f41c126fe631387816aac
Tim Phillips <timmyiscool8@gmail.com>
2021-09-04 11:15:34 -0700

    Finished torches.

--- engine/data/scripts/logic/entities.js
+++ engine/data/scripts/logic/entities.js
@@ -45,7 +45,7 @@ class Entity {
 		this.y = y;
 
 		// Add this entity to the global parent.
-		entities.push(this);
+		entities.entities.push(this);
 	}
 
 	// Placeholder rendering. Can be used to render a blank entity to show errors... Potentially.
@@ -59,6 +59,7 @@ class Torch extends Entity {
 		super(x, y); // Do regular entity stuff.
 
 		// Lighting data.
+		this.initLightStrength = lightStrength; // STore the initial light strength value.
 		this.lightStrength = lightStrength; // The strength of the light this torch emits.
 		this.lightPulses = lightPulses; // Wether or not the light strength of the torch changes slightly to look like a flame growing and shrinking.
 
@@ -71,22 +72,24 @@ class Torch extends Entity {
 		this.solid = false; // Torches can be walked through.
 
 		// Animation data.
-		this.animationMap = new Sheet("spritesheet_torch", {
-			heart: { x: 0, y: 0 },
-			coin: { x: 1, y: 0 },
-		});
+		this.animationMap = new Sheet("spritesheet_torch");
+
+		// Wait for the animation map to load.
+		this.loadMap = () => {
+			this.animation.map = this.animationMap.map;
+		};
 
 		this.animation = {
-			map: this.animationMap,
+			map: this.animationMap.map,
 			name: "loop", // The name of the current animation.
 			frame: 0, // The frame of the current animation.
-			speed: 4, // The speed of the current animation.
+			speed: 1, // The speed of the current animation. Bigger numbers are slower.
 			tick: 0, // The current position in the frame.
 			frameCounts: {
 				// How many frames are in each animation, and at what frame they start.
 				loop: {
 					start: 0,
-					end: 0,
+					end: 2,
 				},
 			},
 		};
@@ -94,6 +97,11 @@ class Torch extends Entity {
 
 	// Animate the torch.
 	animate() {
+		// Check if the animation has been loaded.
+		if (this.animation.map === undefined) {
+			this.loadMap();
+		}
+
 		// Update animation steps.
 		this.animation.tick++; // Add to the tick.
 
@@ -101,6 +109,11 @@ class Torch extends Entity {
 		if (this.animation.tick > this.animation.speed) {
 			this.animation.tick = 0;
 			this.animation.frame++;
+
+			// Flicker the light if we should.
+			if (this.lightPulses && this.animation.frame % 3) {
+				this.lightStrength += randRange(-1, 1);
+			}
 		}
 
 		// If we have finished an animation, restart it.
@@ -116,30 +129,33 @@ class Torch extends Entity {
 			this.animation.frame =
 				this.animation.frameCounts[this.animation.name].start;
 		}
-
-		// Animate attacking.
-		this.animateAttacking();
 	}
 
 	logic() {
 		// Run animations.
 		this.animate();
+
+		// Check that the light strength isn't out of bounds.
+		if (this.lightStrength < this.initLightStrength / 2) {
+			this.lightStrength = this.initLightStrength / 2;
+		} else if (this.lightStrength > this.initLightStrength) {
+			this.lightStrength = this.initLightStrength;
+		}
 	}
 
 	render(ctx) {
-        // We attempt to render. The only reason this would fail is if the spritesheet hasn't loaded yet. Which for the first few frames after the engine starts, it hasn't.
+		// We attempt to render. The only reason this would fail is if the spritesheet hasn't loaded yet. Which for the first few frames after the engine starts, it hasn't.
 		try {
 			ctx.beginPath();
 
 			ctx.drawImage(
 				this.animation.map, // The tilemap image.
-				this.animation.frame * 8 +
-					(this.animation.direction === 1 && 32), // The x and y sub-coordinates to grab the animation's texture from the image.
+				this.animation.frame * 8, // The x and y sub-coordinates to grab the animation's texture from the image.
 				0,
 				8, // The 8x8 pixel dimensions of that sub-image.
 				8,
-				this.x - this.camera.x, // Proper placement of the animation on screen.
-				this.y - this.camera.y,
+				this.x - player.camera.x, // Proper placement of the animation on screen.
+				this.y - player.camera.y,
 				8, // The size of the animation, as drawn on screen.
 				8
 			);
@@ -152,4 +168,5 @@ class Torch extends Entity {
 }
 
 // Debugging:
-let t = new Torch(8, 8, 10, false);
+let t = new Torch(8, 8, 1, true);
+console.log(t);
